971345c51d752fe2064924060b6c951864007ec2
Priyanagarajan06 <166126305+Priyanagarajan06@users.noreply.github.com>
2025-07-04 23:23:07 +0530

    Update AlphabeticalOrder.java

--- AlphabeticalOrder.java
+++ AlphabeticalOrder.java
@@ -3,7 +3,9 @@ public class AlphabeticalOrder{
 char c1,c2;
 void character(){
 Scanner sc = new Scanner(System.in);
+System.out.println("Enter c1:");
 c1 = sc.nextLine().charAt(0);
+System.out.println("Enter c2:");
 c2 = sc.nextLine().charAt(0);
 if(c1<c2){
 System.out.println(c1+","+c2);
